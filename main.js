d4cf7a3b7c438c0edb9ce2fa2e41b85af0a28523
Pedro Henrique <pedrinfnn@gmail.com>
2022-06-21 21:09:34 -0300

    :fire:

---
+++ main.js
@@ -0,0 +1,18 @@
+const turnOn = document.getElementById("turnOn");
+const turnOff = document.getElementById("turnOff");
+const lamp = document.getElementById("lamp");
+
+function lampOn() {
+  lamp.src = "./assets/ligada.jpg";
+}
+function lampOff() {
+  lamp.src = "./assets/desligada.jpg";
+}
+function lampBroken() {
+  lamp.src = "./assets/quebrada.jpg";
+}
+turnOn.addEventListener("click", lampOn);
+turnOff.addEventListener("click", lampOff);
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOff);
+lamp.addEventListener("dblclick", lampBroken);
